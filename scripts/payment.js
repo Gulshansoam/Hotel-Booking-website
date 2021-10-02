649e7d50e57a4977a924715a2f0f3f4ab8abd7f0
Gulshan <gulshansoam3172gmail.com>
2021-10-02 14:53:30 +0530

    Checkpoint15

--- scripts/payment.js
+++ scripts/payment.js
@@ -1,43 +1,50 @@
-function store() {
-    var Username = document.getElementById("name").value;
-    var password = document.getElementById("password").value;
-    localStorage.setItem(Username, password);
-  }
+let urlParams = new URLSearchParams(window.location.search);
+const API_URL = "https://travel-advisor.p.rapidapi.com/";
+const tripAdvisorHost = "travel-advisor.p.rapidapi.com";
+const tripAdvisorKey = "2e7636770amsh70de13951eb8369p128fc1jsnc3a74384824e";
 
-  function loginM() {
-    store();
-    var Username = document.getElementById("name").value;
-    var password = document.getElementById("password").value;
-    if (Username == "admin" && password == "admin") {
-      window.alert("Login Successful");
-      console.log(document.getElementById("logout-button").innerHTML);
-      document.getElementById("logout-button").innerHTML = "LOGOUT";
-      document.getElementById("logout-button").setAttribute("data-toggle", "none");
-      document.getElementById("ena").removeAttribute("disabled");
 
-    }
-  }
-  function logout() {
-    var data = document.getElementById("logout-button");
+let fetchAPI = () => {
+    let xhr = new XMLHttpRequest();
 
-    if (data.innerHTML == "LOGOUT" || data.innerHTML == "LOGIN") {
-      data.innerHTML = "LOGIN";
-      data.setAttribute("data-toggle", "modal");
-      document.getElementById("close").setAttribute("data-dismiss","modal");
-      document.getElementById("ena").setAttribute("disabled",true);
-      localStorage.clear();
-    }
-  }
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            let result = JSON.parse(this.responseText).data[0];
 
-  function pay(){
-    var data = document.getElementById("logout-button");
+            let toDate = new Date(urlParams.get('toDate'));
+            let fromDate = new Date(urlParams.get('fromDate'));
+            let days = (toDate - fromDate)/(24*60*60*1000);
 
-    if (data.innerHTML == "LOGOUT"){
+            document.getElementById("hotel-image").src = result.photo.images.medium.url;
+            document.getElementById("hotel-name").innerText = result.name;
+            document.getElementById("ranking").innerHTML = "<b>" + result.ranking + "</b>";
+            document.getElementById("address").innerText = result.address;
+            document.getElementById("name").innerHTML = "<strong class='heading'>Name:</strong>&nbsp;" + urlParams.get('name');
+            document.getElementById("adult").innerHTML = "<strong class='heading'>Number of Adults:</strong>&nbsp;" + urlParams.get('adult');
+            document.getElementById("from-date").innerHTML = "<strong class='heading'>Check-in Date:</strong>&nbsp;" + urlParams.get('fromDate');
+            document.getElementById("to-date").innerHTML =  "<strong class='heading'>Check-out Date:</strong>&nbsp;" + urlParams.get('toDate');
+            document.getElementById("tariff").innerHTML = "<strong class='heading'>Tariff Breakdown:</strong>&nbsp;Rs.1000 x " + urlParams.get('adult') + " Adults x " + days + " Nights";
+            document.getElementById("amount").innerHTML = "<strong class='heading'>Total Amount:</strong>&nbsp;" + urlParams.get('price');
+            disableLoader();
+        }
+    });
 
-        document.getElementById("ena").removeAttribute("disabled");
-        alert("Hi your booking is sucessful");
-    }else{
+    xhr.open("GET", API_URL + "hotels/get-details?lang=en_US&location_id=" + urlParams.get('id'));
+    xhr.setRequestHeader("x-rapidapi-host", tripAdvisorHost);
+    xhr.setRequestHeader("x-rapidapi-key", tripAdvisorKey);
 
-        document.getElementById("ena").setAttribute("disabled",true);
-    }
-  } 
+    xhr.send();
+}
+
+fetchAPI();
+
+if (!isLogin || isLogin === 'false') {
+    document.getElementById('pay-now-button').disabled = true;
+} else if (isLogin === 'true') {
+document.getElementById('pay-now-button').disabled = false;
+}
+
+let payNow = e => {
+    e.preventDefault();
+    alert('Hi your booking is successfull!');
+};
